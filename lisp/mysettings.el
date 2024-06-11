1197c6cc1b6ac24e7b98bb081398ab005dca3489
oonishi <oonishi@oec.co.jp>
2024-06-11 02:56:30 +0000

    選択範囲を独立したバッファに切り出す

--- lisp/mysettings.el
+++ lisp/mysettings.el
@@ -157,6 +157,23 @@
 
   ;; ビープ音を鳴らさない
   (setq ring-bell-function 'ignore)
+
+  ;; 選択範囲を独立したバッファに切り出す
+  (defun my/edit-region-in-indirect-buffer (start end)
+    "Edit the region specified by START and END in an indirect buffer.
+     Only the region is visible in the indirect buffer, thanks to `narrow-to-region`."
+    (interactive "r")
+    (let ((buffer-name (generate-new-buffer-name "*Indirect Region Edit*"))
+          (origin-buffer (current-buffer)))
+      ;; 1. 間接バッファを作成
+      (with-current-buffer (make-indirect-buffer origin-buffer buffer-name t)
+        ;; 2. 新しいバッファで選択範囲を狭める
+        (narrow-to-region start end)
+        ;; 3. 新しいバッファを表示し、ユーザーに編集させる
+        (switch-to-buffer-other-window (current-buffer))
+        ;; オプション: この時点で追加の設定やモードの変更を行う
+        )))
+  
 )
 
 (leaf expand-region
